d5b1bc6e43535637e0fcec0dfee0a95fddd9f72d
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-09-16 19:43:30 -0300

    feat(string-1): Resolve first list

--- basic/string1.py
+++ basic/string1.py
@@ -25,7 +25,7 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
     # +++your code here+++
-    return
+    return f"Number of donuts: {'many' if count >= 10 else str(count)}"
 
 
 # B. both_ends
@@ -35,8 +35,7 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
     # +++your code here+++
-    return
-
+    return f"{s[:2]}{s[-2:]}" if len(s) > 2 else ''
 
 # C. fix_start
 # Given a string s, return a string
@@ -49,7 +48,8 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
     # +++your code here+++
-    return
+    first, rest = s[0], s[1:]
+    return f"{first}{rest.replace(first, '*')}"
 
 
 # D. MixUp
@@ -61,7 +61,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
     # +++your code here+++
-    return
+    return f"{b[:2]}{a[2:]} {a[:2]}{b[2:]}"
 
 
 # Provided simple test() function used in main() to print
