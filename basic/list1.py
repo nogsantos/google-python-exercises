774925cb42222aa5e9ee47cb136d8f2a3c4c60f3
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-09-25 23:07:42 -0300

    feat(exercise): List one

--- basic/list1.py
+++ basic/list1.py
@@ -31,26 +31,36 @@ def match_ends(words):
     return count
 
 
-# B. front_x
-# Given a list of strings, return a list with the strings
-# in sorted order, except group all the strings that begin with 'x' first.
-# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
-# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
-# Hint: this can be done by making 2 lists and sorting each of them
-# before combining them.
 def front_x(words):
-    # +++your code here+++
-    return
+    """
+    B. front_x
+    Given a list of strings, return a list with the strings
+    in sorted order, except group all the strings that begin with 'x' first.
+    e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+    ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+    Hint: this can be done by making 2 lists and sorting each of them
+    before combining them.
+    """
+    begin_x = []
+    ordinary_list = []
+    for i in words:
+        if i[0] == 'x':
+            begin_x.append(i)
+        else:
+            ordinary_list.append(i)
+    return sorted(begin_x) + sorted(ordinary_list)
 
 
-# C. sort_last
-# Given a list of non-empty tuples, return a list sorted in increasing
-# order by the last element in each tuple.
-# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
-# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
-# Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
+    """
+    C. sort_last
+    Given a list of non-empty tuples, return a list sorted in increasing
+    order by the last element in each tuple.
+    e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+    [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+    Hint: use a custom key= function to extract the last element
+    form each tuple.
+    """
     return
 
 
